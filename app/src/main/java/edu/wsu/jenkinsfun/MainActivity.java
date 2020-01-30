a0a82a5d02dd8d232d73b8642a6dc9547b1aea0f
halaali <aliha2016@gmail.com>
2020-01-30 12:52:49 -0500

    added bird string

--- app/src/main/java/edu/wsu/jenkinsfun/MainActivity.java
+++ app/src/main/java/edu/wsu/jenkinsfun/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private String bird;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
