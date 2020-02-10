606df36228ff83af73d17521f034250347cb7b09
halaali <aliha2016@gmail.com>
2020-02-10 16:32:34 -0500

    added test file

---
+++ app/src/test/java/edu/wsu/jenkinsfun/MainTest.java
@@ -0,0 +1,12 @@
+package edu.wsu.jenkinsfun;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(10, 8 + 2);
+    }
+}
